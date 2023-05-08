ec8a28b714d4e3f0b8df03eb8c874d70f70014e3
enzomiranda1 <100974075+enzomiranda1@users.noreply.github.com>
2023-05-07 22:32:58 -0300

    Update Entropia_de_Dados.r

--- Entropia_de_Dados.r
+++ Entropia_de_Dados.r
@@ -1,4 +1,3 @@
-# definindo as funções de entropia
 entropy <- function(x) {
   probs <- table(x)/length(x)
   -sum(probs * log2(probs))
@@ -8,14 +7,11 @@ max_entropy <- function(n) {
   log2(n)
 }
 
-# carregando o pacote e conjunto de dados
 library(datasets)
 data("ChickWeight")
 
-# calculando a entropia de dados para a variável "Diet"
 diet_entropy <- entropy(ChickWeight$Diet)
 cat("Entropia de dados para a variável 'Diet':", diet_entropy, "\n")
 
-# calculando a entropia máxima para a variável "Diet"
 diet_max_entropy <- max_entropy(length(unique(ChickWeight$Diet)))
 cat("Entropia máxima para a variável 'Diet':", diet_max_entropy, "\n")
